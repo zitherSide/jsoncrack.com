7e7da99d832292b30d061bde77ddac977b9f31f5
zitherSide <takuyakotoyori@akane.waseda.jp>
2022-10-23 07:22:21 +0900

    made electron

---
+++ electron-src/index.ts
@@ -0,0 +1,35 @@
+import { join } from "path";
+import { format } from "url";
+import { BrowserWindow, app, shell } from "electron";
+import isDev from "electron-is-dev";
+import prepareNext from "electron-next";
+
+app.on("ready", async () => {
+  await prepareNext(".");
+
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: join(__dirname, "preload.ts"),
+    },
+  });
+
+  mainWindow.webContents.on("new-window", (event, url) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
+  const url = isDev
+    ? "http://localhost:8000/editor"
+    : format({
+        pathname: join(__dirname, "../out/editor.html"),
+        protocol: "file:",
+        slashes: true,
+      });
+
+  mainWindow.loadURL(url);
+});
+
+app.on("window-all-closed", app.quit);
